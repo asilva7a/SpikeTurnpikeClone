0a883f8cec6df84d87d9ca076e297a464348ea7a
Alexander Silvagnoli <silva7a@cmich.edu>
2024-10-23 22:13:29 +0000

    optimized calculation of firing rate

--- TestingAndDevelopment/FiringRateComparison/calculateFiringRate_fun.m
+++ TestingAndDevelopment/FiringRateComparison/calculateFiringRate_fun.m
@@ -1,17 +1,17 @@
 function avg_FR = calculate_FR(spikeTimes, StartTime, endTime, binSize)
-    %Calculate firing rate by dividing the period into bins and counting spikes
+    % Calculate firing rate by dividing the period into bins and counting spikes
     intervalBounds = StartTime:binSize:endTime;
-    binnedFRs = [];
+    numBins = length(intervalBounds) - 1;
+    binned_FRs = zeros(1, numBins);
 
-    for ii = 1:length(intervalBounds)-1
-        % Count spikes within the current binned
-        n_spikes = length(spikeTimes(spikeTimes >= intervalBounds(ii) & spikeTimes < intervalBounds(ii+1)));
-        FR_in_bin = n_spikes / binSize;
-        binned_FRs(ii) = FR_in_bin;
+    for ii = 1:numBins
+        % Count spikes within the current bin
+        n_spikes = sum(spikeTimes >= intervalBounds(ii) & spikeTimes < intervalBounds(ii+1));
+        binned_FRs(ii) = n_spikes / binSize;
     end
 
-    %Return the average firing rate across bins
-    if ~isempty(binned_FRs)
+    % Return the average firing rate across bins
+    if numBins > 0
         avg_FR = mean(binned_FRs);
     else
         avg_FR = 0;
