1b0e5cb4d92cfd4e19ae7fc749bcd94b820ad58e
silva7a-local <silva7a-local@med-156503>
2024-10-27 15:53:59 -0400

    added minor improvements for clarity

--- TestingAndDevelopment/FiringRateComparison/plot_unit_PSTH_fun.m
+++ TestingAndDevelopment/FiringRateComparison/plot_unit_PSTH_fun.m
@@ -1,27 +1,44 @@
 function plot_unit_PSTH_fun(psthData)
-    groups = fieldnames(psthData);
+    % Plot individual PSTHs for each group and unit, color-coded by response type.
+    
+    groups = fieldnames(psthData);  % Extract all groups from psthData
+
     for g = 1:length(groups)
-        groupName = groups{g};
-        units = fieldnames(psthData.(groupName));
-        
+        groupName = groups{g};  % Current group name
+        units = fieldnames(psthData.(groupName));  % Extract units for this group
+
+        % Create a figure for this group
         figure('Name', ['PSTHs - ', groupName], 'NumberTitle', 'off');
         hold on;
-        
-        for u = 1:length(units)
-            unit = units{u};
-            psth = psthData.(groupName).(unit).PSTH;
-            responseType = psthData.(groupName).(unit).ResponseType;
 
-            % Define color based on response type
+        for u = 1:length(units)
+            unitName = units{u};  % Current unit name
+            unitData = psthData.(groupName).(unitName);
+
+            % Extract PSTH and response type
+            psth = unitData.PSTH;
+            responseType = unitData.ResponseType;
+
+            % Define plot color based on response type
             color = getColorForResponseType(responseType);
-            
-            % Plot the PSTH
+
+            % Plot the PSTH for this unit
             plot(psth, 'Color', color, 'LineWidth', 1.5);
+
+            % Add a legend entry for this unit
+            legendInfo{u} = sprintf('%s - %s', unitName, responseType);
         end
+
+        % Customize plot appearance
+        title(['PSTHs for ', groupName]);
+        xlabel('Time Bins');
+        ylabel('Firing Rate (Hz)');
+        legend(legendInfo, 'Location', 'best');
         hold off;
     end
 end
 
+% Helper function to define color based on response type
 function color = getColorForResponseType(responseType)
     switch responseType
         case 'Increased'
@@ -32,3 +49,4 @@ function color = getColorForResponseType(responseType)
             color = [0, 0, 0];  % Black
     end
 end
+
