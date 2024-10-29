42e15b89edeaf0c6e402dfaf54788bf481beac30
asilva7a <150170376+asilva7a@users.noreply.github.com>
2024-10-29 11:27:59 -0400

    finalized lines for pulling data

--- TestingAndDevelopment/firingRateComparisonV2/extractUnitData.m
+++ TestingAndDevelopment/firingRateComparisonV2/extractUnitData.m
@@ -1,41 +1,37 @@
 function cellData_Struct = extractUnitData(all_data)
 % extractUnitData 
 %   Does what is says on the box
-%   Pulls out the firing rate from the all_data struct for one unit    
+%   Extracts variables from all_data struct to create cellData struct
+%   Saves cellData to user specified directory for later use
+%   Clears workspace
 
 %% Extract relevant data from all_data struct
-    % Get group name (assuming Pvalb is the 2nd group)
-    groupName = fieldnames(all_data); % List field names in the first level
-    group = groupName{2}; % Extract the second group (assuming it's 'Pvalb')
+    % Get group name 
+    groupName = fieldnames(all_data); % List field
+    group = groupName{2}; % Extract Pvalb group (assuming 2nd)
 
-    % Get recording name (assuming 2nd recording)
+    % Get recording name
     recordingName = fieldnames(all_data.(group)); % List recording names
-    recording = recordingName{2}; % Extract the second recording name
+    recording = recordingName{2}; % Extract 'pvalb_hctztreat_0008_rec1'
 
     % Get unit info (assuming unit 'cid314' is the 32nd unit)
     unitID = fieldnames(all_data.(group).(recording)); % List unit IDs
-    unit = unitID{32}; % Extract the 32nd unit (assuming it's 'cid314')
+    unit = unitID{32}; % extract unit data cid314
 
-    % Now extract the unit data
+    % Extract the unit data
     unitData = all_data.(group).(recording).(unit);
 
-    % Pre-load original cell data
-    cellData = all_data.Pvalb.pvalb_hctztreat_0008_rec1.cid314;
-
+%% Generate Struct for storing all_data and new data
     % Generate struct for storing extracted data and future analysis
-    cellData_Struct = struct();
-
-    % Label fields for struct
-    groupName = fieldnames(all_data);
-    recordingName = fieldnames(cellData.(groupName));
-    unitID = fieldnames(cellData.(groupName).(recordingName));
-    unitData = cellData.(groupName).(recordingName).(unitID);    
+    cellData_Struct = struct(); 
 
     % Extract Data
     spikeTimes = unitData.spikeTimes;
     samplingFrequency = unitData.Sampling_Frequency;
     cellType = unitData.Cell_Type;
     isSingleUnit = unitData.IsSingleUnit;
+    durationInSeconds = unitData.Recording_Duration;
+
         
      % Store all relevant data in the responsive_units_struct
         cellData_Struct.(groupName).(recordingName).(unitID) = struct( ...
