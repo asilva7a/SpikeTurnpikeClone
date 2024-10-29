eb17fe8d55e53670e5e4ef31d56280bf44bc9ec1
asilva7a <150170376+asilva7a@users.noreply.github.com>
2024-10-29 10:21:51 -0400

    Added mainscript for directing the functions of the pipeline

--- TestingAndDevelopment/firingRateComparisonV2/extractUnitData.m
+++ TestingAndDevelopment/firingRateComparisonV2/extractUnitData.m
@@ -1,13 +1,38 @@
-function extractUnitData [outputArg1,outputArg2] = extractUnitData(inputArg1,inputArg2);
-%extractUnitData 
+function cellData_Struct = extractUnitData(all_data)
+% extractUnitData 
 %   Does what is says on the box
 %   Pulls out the firing rate from the all_data struct for one unit
+ 
+    % Pre-load original cell data
+    cellData = all_data.Pvalb.pvalb_hctztreat_0008_rec1.cid314;
 
-% Pre-load original cell data
-load('all_data')
+    % Generate struct for storing extracted data and future analysis
+    cellData_Struct = struct();
 
-cellData = all_data.e;
+    % Label fields for struct
+    groupName = fieldnames(cellData);
+    recordingName = fieldnames(cellData.(groupName));
+    unitID = fieldnames(cellData.(groupName).(recordingName));
+    unitData = cellData.(groupName).(recordingName).(unitID);    
 
+    % Extract Data
+    spikeTimes = unitData.spikeTimes;
+    samplingFrequency = unitData.Sampling_Frequency;
+    cellType = unitData.Cell_Type;
+    isSingleUnit = unitData.IsSingleUnit;
+        
+     % Store all relevant data in the responsive_units_struct
+        cellData_Struct.(groupName).(recordingName).(unitID) = struct( ...
+            'spikeTimes_All', spikeTimes, ...
+            'samplingFrequency', samplingFrequency, ...
+            'cellType', cellType, ...
+            'isSingleUnit', isSingleUnit, ...
+            'frAvg', mean(firingRate), ...
+            'pValue', pValue, ...
+            'responseType', responseType, ...
+            'recording', recordingName, ...
+            'binWidth', binWidth, ...
+            'recordingLength', Recording_Duration ...
+             );
 
-outputArg2 = inputArg2;
 end
