a7d639e7ec91e683fb6ba19c34ea7af203b9ac97
asilva7a <150170376+asilva7a@users.noreply.github.com>
2024-10-29 12:13:46 -0400

    updated saving strat

--- TestingAndDevelopment/firingRateComparisonV2/frTreatmentAnalysisMain.m
+++ TestingAndDevelopment/firingRateComparisonV2/frTreatmentAnalysisMain.m
@@ -16,17 +16,35 @@
 % analyze_units
 % Main script for analysing the single unit data
 
+%% Get Default Directory for data and storage
+
+% Define the folder and file name for all_data
+dataFolder = 'C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData';
+fileName = 'all_data.mat';
+filePath = fullfile(dataFolder, fileName);
+
+% Load the file
+if exist(filePath, "file") == 2;
+    load(filePath);
+    disp('File loaded successfully!');
+else
+    error('File not found: %s', filePath);
+end
+
+% Define folder for saving 
+saveFolder = 'C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData';
+
 %% Get User Input for directory
 % Prompt the user to select directory with all_data struct
-    dataDir = uigetdir(pwd, 'Select Directory to Save Files');
-    if dataDir == 0
-        error('No directory selected. Exiting script.');
-    end
+    %dataDir = uigetdir(pwd, 'Select Directory to Save Files');
+    %if dataDir == 0
+        %error('No directory selected. Exiting script.');
+    %end
 % Prompt the user to select a directory to save files
-    saveDir = uigetdir(pwd, 'Select Directory to Save Files');
-    if saveDir == 0
-        error('No directory selected. Exiting script.');
-    end
+    %saveDir = uigetdir(pwd, 'Select Directory to Save Files');
+    %if saveDir == 0
+        %error('No directory selected. Exiting script.');
+    %end
 
 %% Extract Unit Data Into Data Structure (in progress)
 
@@ -34,7 +52,7 @@
 load('all_data.mat')
 
 % Extract relevant fields from all_data struct
-extractUnitData(all_data);
+extractUnitData(all_data,saveFolder);
 
 %% Analysis (to do)
 
