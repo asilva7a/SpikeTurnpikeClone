260e170aa6061fbdb2eeedafa28c8733460dd59a
asilva7a <150170376+asilva7a@users.noreply.github.com>
2024-10-29 10:31:40 -0400

    added to do list

--- TestingAndDevelopment/firingRateComparisonV2/frTreatmentAnalysisMain.m
+++ TestingAndDevelopment/firingRateComparisonV2/frTreatmentAnalysisMain.m
@@ -12,6 +12,16 @@
     if saveDir == 0
         error('No directory selected. Exiting script.');
     end
+%% Extract Unit Data Into Data Structure (in progress)
 
 % Load data
 load('all_data.mat')
+
+% Extract relevant fields from all_data struct
+extractUnitData(all_data);
+
+%% Analysis (to do)
+
+%% Plotting (to do)
+
+
