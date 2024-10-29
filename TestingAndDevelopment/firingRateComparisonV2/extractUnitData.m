53cab7475607f7e805d005a177e00497dffd4242
asilva7a <150170376+asilva7a@users.noreply.github.com>
2024-10-29 09:28:26 -0400

    Merge commit '79974831deae133ed16298815d9c987ce5a5e6ac'

--- TestingAndDevelopment/firingRateComparisonV2/extractUnitData.m
+++ TestingAndDevelopment/firingRateComparisonV2/extractUnitData.m
@@ -1,6 +1,13 @@
-function extractUnitData [outputArg1,outputArg2] = untitled3(inputArg1,inputArg2);
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here 
-outputArg1 = inputArg1;
+function extractUnitData [outputArg1,outputArg2] = extractUnitData(inputArg1,inputArg2);
+%extractUnitData 
+%   Does what is says on the box
+%   Pulls out the firing rate from the all_data struct for one unit
+
+% Pre-load original cell data
+load('all_data')
+
+cellData = all_data.e;
+
+
 outputArg2 = inputArg2;
 end
