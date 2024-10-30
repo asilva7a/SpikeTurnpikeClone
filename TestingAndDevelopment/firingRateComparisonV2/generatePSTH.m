adce24e10e76fe74aa7d2b2910da5a2a19a05aae
asilva7a <150170376+asilva7a@users.noreply.github.com>
2024-10-29 21:21:28 -0400

    deleted LLM code and replaced with original split script; will need to refactor to work with data

--- TestingAndDevelopment/firingRateComparisonV2/generatePSTH.m
+++ TestingAndDevelopment/firingRateComparisonV2/generatePSTH.m
@@ -10,26 +10,27 @@ function [fullPSTH, binEdges] = generatePSTH(cellDataStruct)
     
     % Set binning based on recording length
     recordingLength = unitData.RecordingDuration; % Recording duration in seconds
-    binWidth = unitData.binWidth;  % Flexible for full recording
+    binWidth = unitData.binWidth;  % bin width in seconds
     
-    % Calculate bin edges using the helper function
+     % Calculate bin edges using the helper function
     binEdges = edgeCalculator(0, binWidth, recordingLength);  % Generate bin edges
-    
-    % Compute full PSTH
-    if isempty(spikeTimes) % Handling condition for empty data to prevent errors
-        fullPSTH = zeros(1, length(binEdges) - 1);
-    else
-        fullPSTH = histcounts(spikeTimes, binEdges)/ binWidth;
+
+    %Split Spike Data into Bins
+    splitdata = zeros(floor(bin),floor(lastbin)); % prealocate
+    start_edge = edge(1:lastbin);
+    end_edge = start_edge+bin-1; % add bin amount after subtracting first point
+    for i = 1:lastbin
+        splitdata(:,i) = data(floor(start_edge(i)):floor(end_edge(i))); % extract data
     end
 
-    % Split spike data into bins (aligning with bin edges)
-    splitData = splitSpikeData(spikeTimes, binEdges);
+    % Generate PSTH using split data
+    fullPSTH = cellfun(@length, splitData) / binWidth;
+
+    disp(fullPSTH);
 
     % Save PSTH to cellData struct
     cellDataStruct.Pvalb.pvalb_hctztreat_0008_rec1.cid314.psthRaw = fullPSTH;
-    
-    % Debugging: Display the PSTH length
-    fprintf('Generated PSTH with %d bins for unit %s\n', length(fullPSTH), unitData);
+    cellDataStruct.Pvalb.pvalb_hctztreat_0008_rec1.cid314.splitData = splitData;
 
 end
 
@@ -39,26 +40,7 @@ end
         edges = start:binWidth:stop - 1;  % Generate leading edges
     end
     
-    % % Split spike data into bins (need to finish)
-    % function splitData = splitSpikeData(spikeTimes, binEdges)
-    %     if max(spikeTimes) > 0
-    %         if length(spikeTimes)-(edge(end)-1) < bin % if length of data is less than 1 bin more than final starting bin
-    %             lastbin = max(find(length(data)-(edge-1) >= bin));
-    %         else
-    %             lastbin = length(edge);
-    %         end
-    % 
-    % % split the data using the bins
-    %         splitdata = zeros(floor(bin),floor(lastbin)); % prealocate
-    %         start_edge = edge(1:lastbin);
-    %         end_edge = start_edge+bin-1; % add bin amount after subtracting first point
-    %         for i = 1:lastbin
-    %             splitdata(:,i) = data(floor(start_edge(i)):floor(end_edge(i))); % extract data
-    %         end
-    %     end
+    
 
-     % Debugging: Display spikes in each bin
-     for i = 1:numBins
-         fprintf('Bin %d: %d spikes\n', i, length(splitData{i}));
-     end
+        end
     end
