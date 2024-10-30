a55d35b3b6dee23415094717c3bb9efeef9efbdc
asilva7a <150170376+asilva7a@users.noreply.github.com>
2024-10-29 21:26:02 -0400

    copied in code from splitting; will need to refactor to work with loop

--- TestingAndDevelopment/firingRateComparisonV2/generatePSTH.m
+++ TestingAndDevelopment/firingRateComparisonV2/generatePSTH.m
@@ -15,18 +15,25 @@ function [fullPSTH, binEdges] = generatePSTH(cellDataStruct)
      % Calculate bin edges using the helper function
     binEdges = edgeCalculator(0, binWidth, recordingLength);  % Generate bin edges
 
+    % if data array is given
+    if max(spikeTimes)>0
+        if length(spikeTimes)-(edge(end)-1) < bin % if length of data is less than 1 bin more than final starting bin
+            lastbin = find(length(spikeTimes)-(edge-1) >= bin, 1, 'last' );
+        else
+            lastbin = length(edge);
+        end
+    end
+
     %Split Spike Data into Bins
-    splitdata = zeros(floor(bin),floor(lastbin)); % prealocate
-    start_edge = edge(1:lastbin);
+    splitdata = zeros(floor(bin),floor(lastbin)); % prealocate 
+    start_edge = edges(1:lastbin);
     end_edge = start_edge+bin-1; % add bin amount after subtracting first point
     for i = 1:lastbin
         splitdata(:,i) = data(floor(start_edge(i)):floor(end_edge(i))); % extract data
     end
 
     % Generate PSTH using split data
-    fullPSTH = cellfun(@length, splitData) / binWidth;
-
-    disp(fullPSTH);
+    %fullPSTH = 
 
     % Save PSTH to cellData struct
     cellDataStruct.Pvalb.pvalb_hctztreat_0008_rec1.cid314.psthRaw = fullPSTH;
@@ -40,7 +47,3 @@ end
         edges = start:binWidth:stop - 1;  % Generate leading edges
     end
     
-    
-
-        end
-    end
