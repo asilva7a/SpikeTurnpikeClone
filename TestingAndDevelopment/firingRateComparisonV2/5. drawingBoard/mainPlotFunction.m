cc7e1abd50bd7317463039f41b164cf9475a1f13
asilva7a <150170376+asilva7a@users.noreply.github.com>
2024-11-06 00:24:14 -0500

    finalized first subplot

--- TestingAndDevelopment/firingRateComparisonV2/5. drawingBoard/mainPlotFunction.m
+++ TestingAndDevelopment/firingRateComparisonV2/5. drawingBoard/mainPlotFunction.m
@@ -1,12 +1,19 @@
-function mainPlotFunction(cellDataStruct)
-    % Main function to create combined figure with subplots
+function mainPlotFunction(cellDataStruct, figureFolder)
+   
+% Debugging: Load env variables
+load('C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData\TestVariables\figureFolder.mat');
+load('C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData\TestVariables\cellDataStructPath.mat');
+load('C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData\TestVariables\dataFilePath.mat');
+load('C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData\cellDataStruct_backup_2024-11-05_14-09-54.mat');
+
+% Main function to create combined figure with subplots
 
     figure;
     t = tiledlayout(2, 2, 'Padding', 'compact', 'TileSpacing', 'compact'); % Adjust as needed
 
     % Panel 1 - All Units with Grand Average PSTH
     ax1 = subplot(1, 3, 1);
-    plot1 = subPlotAllPSTHsRawWithMean(cellDataStruct, treatmentTime, ax1);
+    subPlotAllPSTHsRawWithMean(cellDataStruct, 1860, ax1);
 
     % ax2 = nexttile(t);
     % plot2 = plotSecondSubplot(ax2, dataStruct); % Call subplot function 2
@@ -18,29 +25,4 @@ function mainPlotFunction(cellDataStruct)
     title(t, 'Combined Figure with Subplots');
 end
 
-function ax = plotFirstSubplot(ax, dataStruct)
-    % Function to plot first subplot
-    axes(ax); % Use specified axis
-    plot(dataStruct.time, dataStruct.variable1, 'b');
-    title('Subplot 1');
-    xlabel('Time (s)');
-    ylabel('Variable 1');
-end
 
-% function ax = plotSecondSubplot(ax, dataStruct)
-%     % Function to plot second subplot
-%     axes(ax);
-%     plot(dataStruct.time, dataStruct.variable2, 'r');
-%     title('Subplot 2');
-%     xlabel('Time (s)');
-%     ylabel('Variable 2');
-% end
-% 
-% function ax = plotThirdSubplot(ax, dataStruct)
-%     % Function to plot third subplot
-%     axes(ax);
-%     plot(dataStruct.time, dataStruct.variable3, 'g');
-%     title('Subplot 3');
-%     xlabel('Time (s)');
-%     ylabel('Variable 3');
-% end
