40115839cba7a6e4225304fd0342b20fe656b52b
asilva7a <150170376+asilva7a@users.noreply.github.com>
2024-11-06 02:53:36 -0500

    changed to support brace indexing

--- TestingAndDevelopment/firingRateComparisonV2/3. supportFunctions/collectResponsiveCIDs.m
+++ TestingAndDevelopment/firingRateComparisonV2/3. supportFunctions/collectResponsiveCIDs.m
@@ -1,9 +1,6 @@
 function responsiveCIDs = collectResponsiveCIDs(cellDataStruct, groupName)
-    % collectResponsiveCIDs: Helper function for poolResponsiveUnitsAndCalculatePSTH.
-    % Collects CIDs (identifiers) of responsive units in a given group.
-    %
-    % Called by:
-    %   - poolResponsiveUnitsAndCalculatePSTH
+    % collectResponsiveCIDs: Collects CIDs of responsive units in a given group.
+    % Each entry in responsiveCIDs should be a cell array in the format: {groupName, recordingName, unitName}.
     %
     % Inputs:
     %   - cellDataStruct: Data structure containing all groups and units.
@@ -23,7 +20,8 @@ function responsiveCIDs = collectResponsiveCIDs(cellDataStruct, groupName)
             unitData = cellDataStruct.(groupName).(recordingNames{r}).(units{u});
             if isfield(unitData, 'responseType') && ...
                (strcmp(unitData.responseType, 'Increased') || strcmp(unitData.responseType, 'Decreased'))
-                responsiveCIDs = [responsiveCIDs; {groupName, recordingNames{r}, units{u}}];
+                % Append in the format {groupName, recordingName, unitName}
+                responsiveCIDs{end+1, 1} = {groupName, recordingNames{r}, units{u}};
             end
         end
     end
--- TestingAndDevelopment/firingRateComparisonV2/3. supportFunctions/fillPSTHData.m
+++ TestingAndDevelopment/firingRateComparisonV2/3. supportFunctions/fillPSTHData.m
@@ -2,12 +2,9 @@ function psthData = fillPSTHData(cellDataStruct, responsiveCIDs, psthData)
     % fillPSTHData: Helper function for poolResponsiveUnitsAndCalculatePSTH.
     % Fills preallocated PSTH data array with smoothed PSTH data for responsive units.
     %
-    % Called by:
-    %   - poolResponsiveUnitsAndCalculatePSTH
-    %
     % Inputs:
     %   - cellDataStruct: Data structure containing all groups and units.
-    %   - responsiveCIDs: List of responsive CIDs.
+    %   - responsiveCIDs: List of responsive CIDs in the format {groupName, recordingName, unitName}.
     %   - psthData: Preallocated matrix to hold PSTH data.
     %
     % Outputs:
@@ -16,7 +13,15 @@ function psthData = fillPSTHData(cellDataStruct, responsiveCIDs, psthData)
     % Iterate through responsive CIDs and fill data
     for i = 1:numel(responsiveCIDs)
         cid = responsiveCIDs{i};
-        unitData = cellDataStruct.(cid{1}).(cid{2}).(cid{3});
-        psthData(i, :) = unitData.psthSmoothed(:)';
+        
+        try
+            % Attempt to access the data using the specified group, recording, and unit names
+            unitData = cellDataStruct.(cid{1}).(cid{2}).(cid{3});
+            psthData(i, :) = unitData.psthSmoothed(:)';
+        catch
+            % Provide a detailed error message if the path is invalid
+            error('Error accessing data in cellDataStruct. Check if group "%s", recording "%s", or unit "%s" exists.', ...
+                  cid{1}, cid{2}, cid{3});
+        end
     end
 end
