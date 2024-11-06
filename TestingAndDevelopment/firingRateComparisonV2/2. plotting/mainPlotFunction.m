a5bfc04ff373d28c1977287280a33ad32660bbc5
silva7a-local <silva7a-local@med-156503>
2024-11-06 10:58:16 -0500

    added third plotting arg

--- TestingAndDevelopment/firingRateComparisonV2/2. plotting/mainPlotFunction.m
+++ TestingAndDevelopment/firingRateComparisonV2/2. plotting/mainPlotFunction.m
@@ -1,10 +1,11 @@
 function mainPlotFunction(cellDataStruct, figureFolder)
    
 % Debugging: Load env variables
-load('C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData\TestVariables\figureFolder.mat');
-load('C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData\TestVariables\cellDataStructPath.mat');
-load('C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData\TestVariables\dataFilePath.mat');
-load('C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData\TestVariables\cellDataStruct_backup_2024-11-05_14-09-54.mat');
+load('/home/silva7a-local/Documents/MATLAB/SpikeTurnpikeClone/TestData/testVariables/cellDataStructPath.mat');
+load('/home/silva7a-local/Documents/MATLAB/SpikeTurnpikeClone/TestData/testVariables/dataFilePath.mat');
+load('/home/silva7a-local/Documents/MATLAB/SpikeTurnpikeClone/TestData/testVariables/dataFolder.mat');
+load('/home/silva7a-local/Documents/MATLAB/SpikeTurnpikeClone/TestData/testVariables/figureFolder.mat');
+load('/home/silva7a-local/Documents/MATLAB/Data/eb_recordings/SpikeStuff/cellDataStruct.mat');
 
 % Main function to create combined figure with subplots
 
@@ -12,12 +13,12 @@ load('C:\Users\adsil\Documents\Repos\SpikeTurnpikeClone\TestData\TestVariables\c
     t = tiledlayout(1, 3, 'Padding', 'compact', 'TileSpacing', 'compact'); % Adjust as needed
 
     % Panel 1 - All Units with Grand Average PSTH
-    ax1 = subplot(1, 3, 1);
-    subPlotAllPSTHsRawWithMean(cellDataStruct, 1860, ax1);
-    
+    % ax1 = subplot(1, 3, 1);
+    % subPlotAllPSTHsRawWithMean(cellDataStruct, 1860, ax1);
+    % 
     % Panel 2 - All Units (smoothed) with Recording Average 
-    ax2 = subplot(1, 3, 2);
-    subPlotAveragePSTHWithResponse(cellDataStruct, ax2);
+    % ax2 = subplot(1, 3, 2);
+    % subPlotAveragePSTHWithResponse(cellDataStruct, ax2);
 
     % Panel 3 - Experimental vs Control
     ax3 = subplot(1,3,3);
