edb5fbf8818da58e656c6cdcef61abb7059d1449
silva7a-local <silva7a-local@med-156503>
2024-11-12 15:52:02 -0500

    added support function to generate figure directories

---
+++ TestingAndDevelopment/firingRateComparisonV2/5. drawingBoard/generateDirectories.m
@@ -0,0 +1,27 @@
+function generateDirectories(cellDataStruct, figureFolder)
+    % Base directory path
+    baseDir = figureFolder;
+    
+    % Get group names
+    groupNames = fieldnames(cellDataStruct);
+    
+    % Loop through each group
+    for g = 1:length(groupNames)
+        groupName = groupNames{g};
+        % Create group directory
+        groupPath = fullfile(baseDir, groupName);
+        mkdir(groupPath);
+        
+        % Get recordings for this group
+        recordings = fieldnames(cellDataStruct.(groupName));
+        
+        % Loop through each recording
+        for r = 1:length(recordings)
+            recordingName = recordings{r};
+            % Create recording directory
+            recordingPath = fullfile(groupPath, recordingName);
+            mkdir(recordingPath);
+
+        end
+    end
+end
