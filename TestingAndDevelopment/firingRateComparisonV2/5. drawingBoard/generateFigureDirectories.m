268ae17c3caa05760d0c428d7820f47f5f2f413a
silva7a-local <silva7a-local@med-156503>
2024-11-12 16:00:05 -0500

    added checks for existing figure folders

--- TestingAndDevelopment/firingRateComparisonV2/5. drawingBoard/generateFigureDirectories.m
+++ TestingAndDevelopment/firingRateComparisonV2/5. drawingBoard/generateFigureDirectories.m
@@ -2,6 +2,12 @@ function generateFigureDirectories(cellDataStruct, figureFolder)
     % Base directory path
     baseDir = figureFolder;
     
+    % Ensure base directory exists
+    if ~isfolder(baseDir)
+        mkdir(baseDir);
+        fprintf('Created new base figures folder: %s\n', baseDir);
+    end
+    
     % Get group names
     groupNames = fieldnames(cellDataStruct);
     
@@ -10,7 +16,10 @@ function generateFigureDirectories(cellDataStruct, figureFolder)
         groupName = groupNames{g};
         % Create group directory
         groupPath = fullfile(baseDir, groupName);
-        mkdir(groupPath);
+        if ~exist(groupPath, 'dir')
+            mkdir(groupPath);
+            fprintf('Created group folder: %s\n', groupName);
+        end
         
         % Get recordings for this group
         recordings = fieldnames(cellDataStruct.(groupName));
@@ -20,8 +29,10 @@ function generateFigureDirectories(cellDataStruct, figureFolder)
             recordingName = recordings{r};
             % Create recording directory
             recordingPath = fullfile(groupPath, recordingName);
-            mkdir(recordingPath);
-
+            if ~exist(recordingPath, 'dir')
+                mkdir(recordingPath);
+                fprintf('Created recording folder: %s\n', recordingName);
+            end       
         end
     end
 end
