1c9e04280f9eafd233b2229627fd62a5b63b741f
silva7a-local <silva7a-local@med-156503>
2024-11-12 14:35:02 -0500

    archived

--- TestingAndDevelopment/FiringRateComparison/percent_change_PSTH_fun.m
+++
@@ -1,5 +0,0 @@
-function percent_change_PSTH(all_data)
-    % Calculate and plot percent change in firing rate for each unit.
-
-    % (This function calculates the percent change PSTHs and plots the mean + SEM.)
-end
--- TestingAndDevelopment/FiringRateComparison/plot_heatmap_fun.m
+++
@@ -1,6 +0,0 @@
-function plot_heatmap(all_data)
-    % Create a ranked heatmap of firing rate changes (percent change and z-score).
-
-    % (This function generates a heatmap with modulated units ranked from 
-    % most positively to most negatively modulated.)
-end
