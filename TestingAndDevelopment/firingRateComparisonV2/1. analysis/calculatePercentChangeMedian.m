f6a670a0556a70a9cafc39966157cc4c2f312b4a
silva7a-local <silva7a-local@med-156503>
2024-11-12 12:09:50 -0500

    changed saving version to -7.3

--- TestingAndDevelopment/firingRateComparisonV2/1. analysis/calculatePercentChangeMedian.m
+++ TestingAndDevelopment/firingRateComparisonV2/1. analysis/calculatePercentChangeMedian.m
@@ -104,7 +104,7 @@ function cellDataStruct = calculatePercentChangeMedian(cellDataStruct, dataFolde
     end
         % Save the updated struct to the specified data file path
         try
-            save(dataFolder, 'cellDataStruct', '-v7');
+            save(dataFolder, 'cellDataStruct', '-v7.3');
             fprintf('Struct saved successfully to: %s\n', dataFolder);
         catch ME
             fprintf('Error saving the file: %s\n', ME.message);
