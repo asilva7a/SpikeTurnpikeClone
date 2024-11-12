41347bd1b3e65b6d0f40390dd24ec679e60704b3
silva7a-local <silva7a-local@med-156503>
2024-11-12 10:04:43 -0500

    changed saving version to 7.3

--- TestingAndDevelopment/firingRateComparisonV2/1. analysis/smoothAllPSTHs.m
+++ TestingAndDevelopment/firingRateComparisonV2/1. analysis/smoothAllPSTHs.m
@@ -54,7 +54,7 @@ function [cellDataStruct] = smoothAllPSTHs(cellDataStruct, dataFolder, windowSiz
 
     % Save the updated struct to the specified data file path
     try
-        save(dataFolder, 'cellDataStruct', '-v7');
+        save(dataFolder, 'cellDataStruct', '-v7.3');
         fprintf('Struct saved successfully to: %s\n', dataFolder);
     catch ME
         fprintf('Error saving the file: %s\n', ME.message);
