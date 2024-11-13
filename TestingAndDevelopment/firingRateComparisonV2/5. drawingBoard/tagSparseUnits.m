da45eb023011d5f6c453f5df59e73e8b8f47591b
silva7a-local <silva7a-local@med-156503>
2024-11-13 13:30:08 -0500

    added loop and conditional default arg

--- TestingAndDevelopment/firingRateComparisonV2/5. drawingBoard/tagSparseUnits.m
+++ TestingAndDevelopment/firingRateComparisonV2/5. drawingBoard/tagSparseUnits.m
@@ -1,15 +1,43 @@
-function [cellDataStruct,sparseUnitsList] = tagSparseUnits(cellDataStruct, minFrRate, frBefore)
+function [cellDataStruct,sparseUnitsList] = tagSparseUnits(cellDataStruct, frBefore, binWidth, minFrRate)
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-preTreatmentFr = frBefore;
 
-
-
-if preTreatmentFr > minFrRate
-    unitID.isSparseUnit = 1;
-else 
-    unitID.isSparseUnit = 0;
+% Set default args
+if nargin < 2 || isempty(minFrRate)
+    minFrRate = 0.5; % set min fr rate to 0.5Hz
 end
 
+% Check if args present
 
+
+% Set window for sampling
+preTreatmentFr = frBefore;
+
+% Normalize firing rate to bin width
+preTreatmentFr = preTreatmentFr*binWidth;
+
+% Loop through groups, recordings, and units
+    % Loop through groups
+    groupNames = fieldnames(cellDataStruct);
+    for g = 1:length(groupNames)
+        groupName = groupNames{g};
+        recordings = fieldnames(cellDataStruct.(groupName));
+        % Loop through recordings
+        for r = 1:length(recordings)
+            recordingName = recordings{r};
+            units = fieldnames(cellDataStruct.(groupName).(recordingName));
+            % Loop through units
+            for u = 1:length(units)
+                unitID = units{u};
+                unitData = cellDataStruct.(groupName).(recordingName).(unitID);
+                    
+                    % Label sparsity
+                    if preTreatmentFr > minFrRate
+                        unitData.isSparseUnit = 1;
+                    else 
+                        unitData.isSparseUnit = 0;
+                    end
+            end     
+        end
+    end % Saving logic not included; output goes to function w/ save block
 end
