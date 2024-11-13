01d8e5ab3e02b33c1f23cf1e98a0f2a53bb01c39
silva7a-local <silva7a-local@med-156503>
2024-11-13 17:17:40 -0500

    added to git

---
+++ TestingAndDevelopment/firingRateComparisonV2/5. drawingBoard/getCleanUnits.m
@@ -0,0 +1,44 @@
+function cellDataStruct = removeTaggedUnits(cellDataStruct)
+    % Get all groups
+    groupNames = fieldnames(cellDataStruct);
+    
+    % Loop through structure
+    for g = 1:length(groupNames)
+        groupName = groupNames{g};
+        recordings = fieldnames(cellDataStruct.(groupName));
+        
+        for r = 1:length(recordings)
+            recordingName = recordings{r};
+            units = fieldnames(cellDataStruct.(groupName).(recordingName));
+            
+            % Track units to remove
+            unitsToRemove = {};
+            
+            for u = 1:length(units)
+                unitID = units{u};
+                unitData = cellDataStruct.(groupName).(recordingName).(unitID);
+                
+                % Check both flags for filtering
+                if unitData.hasSquareWave || unitData.isOutlierExperimental
+                    unitsToRemove{end+1} = unitID;
+                end
+            end
+            
+            % Remove tagged units
+            for u = 1:length(unitsToRemove)
+                cellDataStruct.(groupName).(recordingName) = ...
+                    rmfield(cellDataStruct.(groupName).(recordingName), unitsToRemove{u});
+            end
+            
+            % Remove empty recordings
+            if isempty(fieldnames(cellDataStruct.(groupName).(recordingName)))
+                cellDataStruct.(groupName) = rmfield(cellDataStruct.(groupName), recordingName);
+            end
+        end
+        
+        % Remove empty groups
+        if isempty(fieldnames(cellDataStruct.(groupName)))
+            cellDataStruct = rmfield(cellDataStruct, groupName);
+        end
+    end
+end
