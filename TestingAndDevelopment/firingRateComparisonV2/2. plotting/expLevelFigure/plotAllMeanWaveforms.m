af9489ab122066889c778c1b15cfbfe0d7263065
silva7a-local <silva7a-local@med-156503>
2024-11-20 13:53:29 -0500

    fixed indentation

--- TestingAndDevelopment/firingRateComparisonV2/2. plotting/expLevelFigure/plotAllMeanWaveforms.m
+++ TestingAndDevelopment/firingRateComparisonV2/2. plotting/expLevelFigure/plotAllMeanWaveforms.m
@@ -66,13 +66,13 @@ function plotAllMeanWaveforms(cellDataStruct)
             end
         end
     end
-
+    
     % Print summary
     fprintf('\nSummary:\n');
     fprintf('Total units: %d\n', size(waveforms_all, 2));
     fprintf('Enhanced units: %d\n\n', size(waveforms_enhanced, 2));
     
-        % Plotting
+    % Plotting
     figure('Position', [100 100 1000 800]);
     T = tiledlayout(2,1);
     
@@ -80,7 +80,7 @@ function plotAllMeanWaveforms(cellDataStruct)
     nexttile(T);
     hold on;
     for unitInd = 1:size(waveforms_enhanced,2)
-        p = plot(waveforms_enhanced(:,unitInd), 'Color', [1 0 0, 0.2]); % Red for enhanced
+        p = plot(waveforms_enhanced(:,unitInd), 'Color', [1 0 0, 0.1]); % Reduced alpha to 0.1
         % Data tips for unit identification
         p.DataTipTemplate.DataTipRows(1:2) = [dataTipTextRow("Rec:", repmat(mouseLabels_enhanced(unitInd),size(waveforms_enhanced,1))),...
                                              dataTipTextRow("Cell:", repmat(cellID_Labels_enhanced(unitInd),size(waveforms_enhanced,1)))];
@@ -93,14 +93,14 @@ function plotAllMeanWaveforms(cellDataStruct)
         plot(meanWF_enhanced, 'Color', [1 0 0], 'LineWidth', 2);
     end
     title(sprintf('Enhanced Units Only (n=%d)', size(waveforms_enhanced,2)));
-    xlim([0 120]);  % Set x-axis limit
+    xlim([0 120]); % Set x-axis limit
     hold off;
     
     % Bottom panel: All units
     nexttile(T);
     hold on;
     for unitInd = 1:size(waveforms_all,2)
-        p = plot(waveforms_all(:,unitInd), 'Color', [0.7 0.7 0.7 0.3]);
+        p = plot(waveforms_all(:,unitInd), 'Color', [0.7 0.7 0.7 0.1]); % Reduced alpha to 0.1
         % Data tips for unit identification
         p.DataTipTemplate.DataTipRows(1:2) = [dataTipTextRow("Rec:", repmat(mouseLabels_all(unitInd),size(waveforms_all,1))),...
                                              dataTipTextRow("Cell:", repmat(cellID_Labels_all(unitInd),size(waveforms_all,1)))];
@@ -109,9 +109,9 @@ function plotAllMeanWaveforms(cellDataStruct)
     
     % Plot mean of all waveforms in grey instead of black
     meanWF_all = mean(waveforms_all,2);
-    plot(meanWF_all, 'Color', [0.6 0.6 0.6], 'LineWidth', 2);  % Changed to grey
+    plot(meanWF_all, 'Color', [0.6 0.6 0.6], 'LineWidth', 2); % Changed to grey
     title(sprintf('All Units (n=%d)', size(waveforms_all,2)));
-    xlim([0 120]);  % Set x-axis limit
+    xlim([0 120]); % Set x-axis limit
     hold off;
     
     % Add overall title
@@ -127,7 +127,7 @@ function plotAllMeanWaveforms(cellDataStruct)
         grid on;
         set(gca, 'Layer', 'top', 'GridAlpha', 0.15);
     end
-
+    
     % Save figure
     try
         % Create figure handle
@@ -140,10 +140,8 @@ function plotAllMeanWaveforms(cellDataStruct)
         % Save both .fig and high-resolution .tif
         savefig(fig, fullfile(saveDir, [fileName '.fig']));
         print(fig, fullfile(saveDir, [fileName '.tif']), '-dtiff', '-r300');
-        
         close(fig);
     catch ME
         warning('Save:Error', 'Error saving figure: %s', ME.message);
     end
-    
 end
